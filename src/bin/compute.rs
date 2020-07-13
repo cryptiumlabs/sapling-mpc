4a9877aee6661244e86a56dbb34d9212bfb3f265
joe <joe@cryptium.ch>
2020-07-13 09:51:44 -0700

    change to masp circuits

--- src/bin/compute.rs
+++ src/bin/compute.rs
@@ -20,23 +20,17 @@ fn main() {
     let mut sapling_output = phase2::MPCParameters::read(&mut current_params, false)
         .expect("couldn't deserialize Sapling Output params");
 
-    let mut sprout_joinsplit = phase2::MPCParameters::read(&mut current_params, false)
-        .expect("couldn't deserialize Sprout JoinSplit params");
-
     let rng = &mut rand::OsRng::new().expect("couldn't create RNG");
 
     let h1 = sapling_spend.contribute(rng);
     let h2 = sapling_output.contribute(rng);
-    let h3 = sprout_joinsplit.contribute(rng);
 
     sapling_spend.write(&mut new_params).expect("couldn't write new Sapling Spend params");
     sapling_output.write(&mut new_params).expect("couldn't write new Sapling Spend params");
-    sprout_joinsplit.write(&mut new_params).expect("couldn't write new Sapling Spend params");
 
     let mut h = Blake2b::new(64);
     h.update(&h1);
     h.update(&h2);
-    h.update(&h3);
     let h = h.finalize();
 
     print!("Done!\n\n\
